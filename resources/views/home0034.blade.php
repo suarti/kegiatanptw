399eafa4d3e4899acc4f8047927d370bb74d014a
suarti <66746681+suarti@users.noreply.github.com>
2021-05-22 22:20:43 +0700

    Update home0034.blade.php

--- resources/views/home0034.blade.php
+++ resources/views/home0034.blade.php
@@ -1,14 +1,51 @@
-<!DOCTYPE html>
-<html>
-<head>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
     <title>Suarti</title>
-</head>
-<body>
-    <a href="{{ url('/') }}" class="text-sm text-gray-700 underline">Home</a>
-    <a href="{{ url('/artikel0034') }}" class="text-sm text-gray-700 underline">Artikel</a>
-    <a href="{{ url('/contact0034') }}" class="text-sm text-gray-700 underline">Contact Us</a>
-    <p>Hello Word</p>
-    <p>Hello word</p>
-    <p>Hallo dunia</p>
-</body>
+  </head>
+  <body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<div class= "container">
+    <a class="navbar-brand" href="/">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <ul class="navbar-nav">
+          <a class="nav-item nav-link" href="/artikel0034">Article</a>
+          <a class="nav-item nav-link" href="/contact0034">Contact Us!</a>
+      </ul>
+    </div>
+  </div>
+  </div>
+</nav>
+
+<div class="container">
+<div class="row">
+<div class="col-10">
+<h1>Universitas 17 Agustus 1945</h1>
+</div>
+</div>
+</div>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    -->
+  </body>
 </html>
